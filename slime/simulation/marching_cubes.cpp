6100a37077a4079349c9f835489ae63735bf9a5e
harutea <snowhanami@gmail.com>
2024-07-29 17:47:37 +0900

    implementing marcing cubes

--- slime/simulation/marching_cubes.cpp
+++ slime/simulation/marching_cubes.cpp
@@ -10,4 +10,7 @@ MarchingCubes::MarchingCubes() {}
 
 MarchingCubes::~MarchingCubes() {}
 
+std::pair<uint16_t, uint16_t> CornerIndexFromEdge(uint8_t edge);
+glm::vec3 CoordFromIndex(uint16_t x, uint16_t y, uint16_t z);
+
 glm::vec3 interpolateVertices(glm::vec3 vertexA, glm::vec3 vertexB) {}
--- slime/simulation/marching_cubes.h
+++ slime/simulation/marching_cubes.h
@@ -3,8 +3,9 @@
 
 #include <cstdint>
 #include <glm/glm.hpp>
-#include <vector>
 #include <marching_cubes_tables.h>
+#include <algorithm>
+#include <vector>
 
 namespace slime {
 class MarchingCubes {
@@ -20,6 +21,8 @@ public:
   MarchingCubes();
   ~MarchingCubes();
 
+  std::pair<uint16_t, uint16_t> CornerIndexFromEdge(uint8_t edge);
+  glm::vec3 CoordFromIndex(uint16_t x, uint16_t y, uint16_t z);
   glm::vec3 interpolateVertices(glm::vec3 vertexA, glm::vec3 vertexB);
 
   template <size_t X, size_t Y, size_t Z>
@@ -29,8 +32,8 @@ public:
     std::vector<MarchingCubes::Triangle> triangles;
 
     /* verify if the vertex order is correct */
-    int8_t diff[8][3] = {{0, 0, 0}, {1, 0, 0}, {1, 0, 1}, {0, 0, 1},
-                         {0, 1, 0}, {1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+    const int8_t diff[8][3] = {{0, 0, 0}, {1, 0, 0}, {1, 0, 1}, {0, 0, 1},
+                               {0, 1, 0}, {1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
 
     for (int x = 0; x < X; x++) {
       for (int y = 0; y < Y; y++) {
@@ -45,9 +48,22 @@ public:
           const std::vector<int8_t> &edges =
               MarchingCubesTables::triangulation[tableKey];
 
-          for (int8_t edge : edges) {
-            if (edge == -1)
+          for (int i = 0; i < edges.size(); i += 3) {
+            if (i == -1)
               continue;
+
+            MarchingCubes::Triangle triangle;
+            triangle.v1 = interpolateVertices(
+                CoordFromIndex(CornerIndexFromEdge(edges[i]).first),
+                CoordFromIndex(CornerIndexFromEdge(edges[i]).second));
+            triangle.v2 = interpolateVertices(
+                CoordFromIndex(CornerIndexFromEdge(edges[i + 1]).first),
+                CoordFromIndex(CornerIndexFromEdge(edges[i + 1]).second));
+            triangle.v2 = interpolateVertices(
+                CoordFromIndex(CornerIndexFromEdge(edges[i + 2]).first),
+                CoordFromIndex(CornerIndexFromEdge(edges[i + 2]).second));
+
+            triangles.push_back(triangle);
           }
         }
       }
